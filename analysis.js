e55281dd1afea42f02d825fef6a779864f80e1ba
Vitor Lima <vitorfdl@tago.io>
2021-09-16 12:06:00 -0300

    update to new dashboard template

--- analysis.js
+++ analysis.js
@@ -3,14 +3,12 @@
  ** Creating devices using dashboard
  **
  ** Using an Input Widget in the dashboard, you will be able to create devices in your account.
- ** You can get the dashboard template to use here: http://admin.tago.io/template/5d51ff8c171477001b230b92
+ ** You can get the dashboard template to use here: https://admin.tago.io/template/6143555a314cef001871ec78
  ** Use a dummy HTTPs device with the dashboard.
  **
  ** Environment Variables
  ** In order to use this analysis, you must setup the Environment Variable table.
  **   account_token: Your account token. Check bellow how to get this.
- **   connector_id: Default Connector ID for the device (optional)
- **   network_id: Default Network Server ID for the device (optional)
  **
  ** Steps to generate an account_token:
  ** 1 - Enter the following link: https://admin.tago.io/account/
@@ -21,8 +19,10 @@
  */
 const { Analysis, Account, Utils, Device } = require('@tago-io/sdk');
 
-async function init(context, scope) {
-  if (!scope[0]) return context.log('This analysis must be triggered by a widget.');
+async function startAnalysis(context, scope) {
+  if (!scope[0]) {
+    return context.log('The analysis must be triggered by a widget.');
+  }
 
   context.log('Creating your device');
   // Get the environment variables.
@@ -33,30 +33,40 @@ async function init(context, scope) {
   // Instance the Account class
   const account = new Account({ token: env.account_token });
 
-  // Get the device used for the dashboard.
+  // Get the token of the settings device used in the dashboard, then instance the device class.
+  // We will use this to send the Validation (feedback) to the dashboard.
   const dashboard_dev_token = await Utils.getTokenByName(account, scope[0].origin);
   const dashboard_device = new Device({ token: dashboard_dev_token });
 
-  // Get the variables form_payload and form_port sent by the widget/dashboard.
-  const connector_id = scope.find(x => x.variable === 'connector_id') || { value: env.payload, origin: env.connector_id };
-  const network_id = scope.find(x => x.variable === 'network_id') || { value: env.payload, origin: env.network_id };
+  // Get the variables sent by the widget/dashboard.
+  const network_id = scope.find(x => x.variable === 'device_network');
+  const connector_id = scope.find(x => x.variable === 'device_connector');
   const device_name = scope.find(x => x.variable === 'device_name');
-  const device_serial = scope.find(x => x.variable === 'device_serial');
+  const device_eui = scope.find(x => x.variable === 'device_eui');
 
-  if (!connector_id || !connector_id.value) return context.log('Missing "connector_id" in the data scope.');
-  else if (!network_id || !network_id.value) return context.log('Missing "network_id" in the data scope.');
+  if (!connector_id || !connector_id.value) {
+    return context.log('Missing "device_connector" in the data scope.');
+  } else if (!network_id || !network_id.value) {
+    return context.log('Missing "device_network" in the data scope.');
+  } else if (!device_eui || !device_eui.value) {
+    return context.log('Missing "device_eui" in the data scope.');
+  }
 
   const result = await account.devices.create({
-    name: device_name.value || 'Any name',
-    serie_number: device_serial.value,
+    name: device_name.value,
+    // Serie number is the parameter for device eui, sigfox id, etc..
+    serie_number: device_eui.value,
     tags: [
       // You can add custom tags here.
-      { key: 'tag_key', value: 'tag_value' },
+      { key: 'type', value: 'sensor' },
+      { key: 'device_eui', value: device_eui.value },
     ],
     connector: connector_id.value,
     network: network_id.value,
     active: true,
   }).catch((error) => {
+    // Send the validation to the device.
+    // That way we create an error in the dashboard for feedback.
     dashboard_device.sendData({ variable: 'validation', value: `Error when creating the device ${error}`, metadata: { color: 'red' } });
     throw error;
   });
@@ -65,12 +75,12 @@ async function init(context, scope) {
   account.devices.paramSet(result.device_id, { key: 'param_key', value: '10', sent: false });
 
   // To add any data to the device that was just created:
-  const device = new Device({ token: result.token });
-  device.sendData({ variable: 'temperature', value: 17 });
+  // const device = new Device({ token: result.token });
+  // device.sendData({ variable: 'temperature', value: 17 });
 
   // Send feedback to the dashboard:
-  dashboard_device.sendData({ variable: 'validation', value: 'Device succesfully created!', metadata: { color: 'green' } });
+  dashboard_device.sendData({ variable: 'validation', value: 'Device succesfully created!', metadata: { type: 'success' } });
   context.log(`Device succesfully created. ID: ${result.device_id}`);
 }
 
-module.exports = new Analysis(init);
+module.exports = new Analysis(startAnalysis);
