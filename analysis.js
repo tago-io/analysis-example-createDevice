30aa01ff6fe774e028449ac8175313407f3e9467
Guilherme Costa <guilhermeoliveira@gec.inatel.br>
2022-07-08 09:38:37 -0300

    fix: fixing new bucket type prop

--- analysis.js
+++ analysis.js
@@ -17,14 +17,14 @@
  ** 4 - Generate a new Token with Expires Never.
  ** 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
  */
-const { Analysis, Account, Utils, Device } = require('@tago-io/sdk');
+const { Analysis, Account, Utils, Device } = require("@tago-io/sdk");
 
 async function startAnalysis(context, scope) {
   if (!scope[0]) {
-    return context.log('The analysis must be triggered by a widget.');
+    return context.log("The analysis must be triggered by a widget.");
   }
 
-  context.log('Creating your device');
+  context.log("Creating your device");
   // Get the environment variables.
   const env = Utils.envToJson(context.environment);
   if (!env.account_token) return context.log('Missing "account_token" environment variable');
@@ -39,10 +39,10 @@ async function startAnalysis(context, scope) {
   const dashboard_device = new Device({ token: dashboard_dev_token });
 
   // Get the variables sent by the widget/dashboard.
-  const network_id = scope.find(x => x.variable === 'device_network');
-  const connector_id = scope.find(x => x.variable === 'device_connector');
-  const device_name = scope.find(x => x.variable === 'device_name');
-  const device_eui = scope.find(x => x.variable === 'device_eui');
+  const network_id = scope.find((x) => x.variable === "device_network");
+  const connector_id = scope.find((x) => x.variable === "device_connector");
+  const device_name = scope.find((x) => x.variable === "device_name");
+  const device_eui = scope.find((x) => x.variable === "device_eui");
 
   if (!connector_id || !connector_id.value) {
     return context.log('Missing "device_connector" in the data scope.');
@@ -52,35 +52,39 @@ async function startAnalysis(context, scope) {
     return context.log('Missing "device_eui" in the data scope.');
   }
 
-  const result = await account.devices.create({
-    name: device_name.value,
-    // Serie number is the parameter for device eui, sigfox id, etc..
-    serie_number: device_eui.value,
-    tags: [
-      // You can add custom tags here.
-      { key: 'type', value: 'sensor' },
-      { key: 'device_eui', value: device_eui.value },
-    ],
-    connector: connector_id.value,
-    network: network_id.value,
-    active: true,
-    type: "immutable"
-  }).catch((error) => {
-    // Send the validation to the device.
-    // That way we create an error in the dashboard for feedback.
-    dashboard_device.sendData({ variable: 'validation', value: `Error when creating the device ${error}`, metadata: { color: 'red' } });
-    throw error;
-  });
+  const result = await account.devices
+    .create({
+      name: device_name.value,
+      // Serie number is the parameter for device eui, sigfox id, etc..
+      serie_number: device_eui.value,
+      tags: [
+        // You can add custom tags here.
+        { key: "type", value: "sensor" },
+        { key: "device_eui", value: device_eui.value },
+      ],
+      connector: connector_id.value,
+      network: network_id.value,
+      active: true,
+      type: "immutable",
+      chunk_period: "month", //consider change
+      chunk_retention: 1, //consider change
+    })
+    .catch((error) => {
+      // Send the validation to the device.
+      // That way we create an error in the dashboard for feedback.
+      dashboard_device.sendData({ variable: "validation", value: `Error when creating the device ${error}`, metadata: { color: "red" } });
+      throw error;
+    });
 
   // To add Configuration Parameters to the device:
-  account.devices.paramSet(result.device_id, { key: 'param_key', value: '10', sent: false });
+  account.devices.paramSet(result.device_id, { key: "param_key", value: "10", sent: false });
 
   // To add any data to the device that was just created:
   // const device = new Device({ token: result.token });
   // device.sendData({ variable: 'temperature', value: 17 });
 
   // Send feedback to the dashboard:
-  dashboard_device.sendData({ variable: 'validation', value: 'Device succesfully created!', metadata: { type: 'success' } });
+  dashboard_device.sendData({ variable: "validation", value: "Device succesfully created!", metadata: { type: "success" } });
   context.log(`Device succesfully created. ID: ${result.device_id}`);
 }
 
